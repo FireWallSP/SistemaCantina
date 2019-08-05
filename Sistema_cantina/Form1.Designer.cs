8afc94fe77e573cba2fbe0594d2a1d97ba2fb023
Rafael Viana <rafael_.viana@hotmail.com>
2019-08-05 19:29:58 -0300

    Primeiro programa 2019

---
+++ Sistema_cantina/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Sistema_cantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.lstCaixa = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SISTEMA CANTINA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CÓDIGO DO PRODUTO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "VALOR TOTAL:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(15, 108);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(144, 20);
+            this.txtCodigo.TabIndex = 3;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(15, 150);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(144, 107);
+            this.picImagem.TabIndex = 4;
+            this.picImagem.TabStop = false;
+            // 
+            // lstCaixa
+            // 
+            this.lstCaixa.FormattingEnabled = true;
+            this.lstCaixa.Location = new System.Drawing.Point(198, 108);
+            this.lstCaixa.Name = "lstCaixa";
+            this.lstCaixa.Size = new System.Drawing.Size(120, 147);
+            this.lstCaixa.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 321);
+            this.Controls.Add(this.lstCaixa);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.ListBox lstCaixa;
+    }
+}
+
